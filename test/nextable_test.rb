7223e492a9cf660260757e1aed6c2b1ad00e9296
Thomas Brigham <thomas@thomasbrigham.me>
2016-01-21 16:05:31 -0800

    Nextable test

--- test/nextable_test.rb
+++ test/nextable_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class NextableTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Nextable
+  def test_user_next_record_fetches_user_with_ascending_id
+    mlk = User.create!(name: 'ML King', total_friends: 99, birthday: 'January 15, 1929')
+    mx = User.create!(name: 'Malcom Little', total_friends: 600, birthday: 'February 21, 1965')
+    assert_equal mlk.next_record, mx
   end
 end
