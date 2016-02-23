5d65bbf4aab573b3d9ac7ae4f968f39054734e3f
Thomas Brigham <thomas@thomasbrigham.me>
2016-02-23 00:02:03 -0800

    DRY test variables
    - Utilize @@ variables to DRY up setup across classes
    - Implement outstanding tests as pending

--- test/param_tests/filters_test.rb
+++ test/param_tests/filters_test.rb
@@ -4,30 +4,103 @@ require 'nextable_test'
 module ParamTests
   class FiltersTest < NextableTest
     setup do
-      @first, @second, @third, @fourth, @fifth, @sixth, @seventh = create_seven_users
+      @@first, @@second, @@third, @@fourth, @@fifth, @@sixth, @@seventh = create_seven_users
+      @@june_opts = { filters: { date_of_birth: '1991-06-02' } }
+      @@oct_opts = { filters: { date_of_birth: '1990-10-26' } }
     end
 
-    class NextRecord < NextableTest
-      test "with_jun_2_1991_date_of_birth" do
-        assert_equal @second.next_record(filters: { date_of_birth: '1991-06-02' }), @third
-        assert_equal @fifth.next_record(filters: { date_of_birth: '1991-06-02' }), nil
+    class NextRecord < FiltersTest
+      class DateFilter < NextRecord
+        test "default" do
+          assert_equal @@second.next_record(@@june_opts), @@third
+          assert_equal @@fifth.next_record(@@june_opts), nil
+        end
+
+        test "with cycle" do
+          fifth = @@seventh.next_record(@@oct_opts.merge(cycle: true))
+          assert_equal fifth, @@fifth
+        end
+
+        test "with field" do
+          sixth = @@seventh.next_record(@@oct_opts.merge(field: 'name'))
+          assert_equal sixth, @@sixth
+        end
+      end
+
+      class IntegerFilter < NextRecord
+        test "default" do
+          skip
+        end
+
+        test "with cycle" do
+          skip
+        end
+
+        test "with field" do
+          skip
+        end
+      end
+
+      class StringFilter < NextRecord
+        test "default" do
+          skip
+        end
+
+        test "with cycle" do
+          skip
+        end
+
+        test "with field" do
+          skip
+        end
       end
     end
 
-    test "with_oct_26_1990_date_of_birth" do
-      assert_equal @second.next_record(filters: { date_of_birth: '1990-10-26' }), @fifth
-      assert_equal @fifth.next_record(filters: { date_of_birth: '1990-10-26' }), @sixth
+    class PreviousRecord < FiltersTest
+      class DateFilter < PreviousRecord
+        test "default" do
+          assert_equal @@second.previous_record(@@june_opts), @@first
+          assert_equal @@first.previous_record(@@june_opts), nil
+        end
 
-      assert_equal @sixth.previous_record(filters: { date_of_birth: '1990-10-26' }), @fifth
-      assert_equal @fifth.previous_record(filters: { date_of_birth: '1990-10-26' }), nil
+        test "with cycle" do
+          seventh = @@fifth.previous_record(@@oct_opts.merge(cycle: true))
+          assert_equal seventh, @@seventh
+        end
 
-      # with cycle
-      assert_equal @fifth.previous_record(cycle: true, filters: { date_of_birth: '1990-10-26' }), @seventh
-      assert_equal @seventh.next_record(cycle: true, filters: { date_of_birth: '1990-10-26' }), @fifth
+        test "with field" do
+          sixth = @@fifth.previous_record(@@oct_opts.merge(field: 'name'))
+          assert_equal sixth, @@sixth
+        end
+      end
 
-      # with field param
-      assert_equal @fifth.previous_record(field: 'name', filters: { date_of_birth: '1990-10-26' }), @sixth
-      assert_equal @seventh.next_record(field: 'name', filters: { date_of_birth: '1990-10-26' }), @sixth
+      class IntegerFilter < PreviousRecord
+        test "default" do
+          skip
+        end
+
+        test "with cycle" do
+          skip
+        end
+
+        test "with field" do
+          skip
+        end
+      end
+
+      class StringFilter < PreviousRecord
+        test "default" do
+          skip
+        end
+
+        test "with cycle" do
+          skip
+        end
+
+        test "with field" do
+          skip
+        end
+      end
     end
   end
 end
