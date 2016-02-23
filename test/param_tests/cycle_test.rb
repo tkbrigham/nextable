e62935767c68d609b4057ef646155787a78aaf8b
Thomas Brigham <thomas@thomasbrigham.me>
2016-02-23 00:02:03 -0800

    DRY test variables
    - Utilize @@ variables to DRY up setup across classes
    - Implement outstanding tests as pending

--- test/param_tests/cycle_test.rb
+++ test/param_tests/cycle_test.rb
@@ -3,51 +3,52 @@ require 'nextable_test'
 
 module ParamTests
   class CycleTest < NextableTest
+    setup do
+      @@first, @@second, @@third = create_three_users
+
+      # Aliased for easier reading of individual tests
+      @@feb_1965 = @@six = @@malcolm = @@first
+      @@feb_1942 = @@eight = @@huey = @@second
+      @@jan_1929 = @@seven = @@mlk = @@third
+    end
+
     class NextRecord < CycleTest
       test "default will cycle by id" do
-        @first, @second, @third = create_three_users
-        assert_equal @third.next_record(cycle: true), @first
+        assert_equal @@third.next_record(cycle: true), @@first
       end
 
       test "date field" do
-        @feb_1965, @feb_1942, @jan_1929 = create_three_users
-        jan =  @feb_1965.next_record(field: 'date_of_birth', cycle: true)
-        assert_equal jan, @jan_1929
+        jan =  @@feb_1965.next_record(cycle: true, field: 'date_of_birth')
+        assert_equal jan, @@jan_1929
       end
 
       test "integer field" do
-        @six, @eight, @seven = create_three_users
-        six = @eight.next_record(cycle: true, field: 'total_friends')
-        assert_equal six, @six
+        six = @@eight.next_record(cycle: true, field: 'total_friends')
+        assert_equal six, @@six
       end
 
       test "string field" do
-        @malcolm, @huey, @mlk = create_three_users
-        assert_equal @mlk.next_record(cycle: true, field: 'name'), @huey
+        assert_equal @@mlk.next_record(cycle: true, field: 'name'), @@huey
       end
     end
 
     class PreviousRecord < CycleTest
       test "default will cycle by id" do
-        @first, @second, @third = create_three_users
-        assert_equal @first.previous_record(cycle: true), @third
+        assert_equal @@first.previous_record(cycle: true), @@third
       end
 
       test "date field" do
-        @feb_1965, @feb_1942, @jan_1929 = create_three_users
-        feb = @jan_1929.previous_record(field: 'date_of_birth', cycle: true)
-        assert_equal feb, @feb_1965
+        feb = @@jan_1929.previous_record(cycle: true, field: 'date_of_birth')
+        assert_equal feb, @@feb_1965
       end
 
       test "integer field" do
-        @six, @eight, @seven = create_three_users
-        eight = @six.previous_record(cycle: true, field: 'total_friends')
-        assert_equal eight, @eight
+        eight = @@six.previous_record(cycle: true, field: 'total_friends')
+        assert_equal eight, @@eight
       end
 
       test "string field" do
-        @malcolm, @huey, @mlk = create_three_users
-        assert_equal @huey.previous_record(cycle: true, field: 'name'), @mlk
+        assert_equal @@huey.previous_record(cycle: true, field: 'name'), @@mlk
       end
     end
   end
--- test/param_tests/field_test.rb
+++ test/param_tests/field_test.rb
@@ -3,6 +3,15 @@ require 'nextable_test'
 
 module ParamTests
   class FieldTest < NextableTest
+    setup do
+      @@feb_1965, @@feb_1942, @@jan_1929 = create_three_users
+
+      # Aliased for easier reading of individual tests
+      @@ten_am = @@six = @@malcolm = @@feb_1965
+      @@nine_am = @@eight = @@huey = @@feb_1942
+      @@eleven_am = @@seven = @@mlk = @@jan_1929
+    end
+
     ####
     ## Next Record
     ####
@@ -10,53 +19,45 @@ module ParamTests
     class NextRecord < FieldTest
       class WithDateField < NextRecord
         test "defaults" do
-          @feb_1965, @feb_1942, @jan_1929 = create_three_users
-          assert_equal @jan_1929.next_record(field: 'date_of_birth'), @feb_1942
-          assert_equal @feb_1942.next_record(field: 'date_of_birth'), @feb_1965
+          assert_equal @@jan_1929.next_record(field: 'date_of_birth'), @@feb_1942
+          assert_equal @@feb_1942.next_record(field: 'date_of_birth'), @@feb_1965
         end
 
         test "last record returns nil" do
-          @feb_1965, @feb_1942, @jan_1929 = create_three_users
-          assert_equal @feb_1965.next_record(field: 'date_of_birth'), nil
+          assert_equal @@feb_1965.next_record(field: 'date_of_birth'), nil
         end
       end
 
       class WithTimeField < NextRecord
         test "defaults" do
-          @ten_am, @nine_am, @eleven_am = create_three_users
-          assert_equal @nine_am.next_record(field: 'time_of_birth'), @ten_am
-          assert_equal @ten_am.next_record(field: 'time_of_birth'), @eleven_am
+          assert_equal @@nine_am.next_record(field: 'time_of_birth'), @@ten_am
+          assert_equal @@ten_am.next_record(field: 'time_of_birth'), @@eleven_am
         end
 
         test "last record returns nil" do
-          @ten_am, @nine_am, @eleven_am = create_three_users
-          assert_equal @eleven_am.next_record(field: 'time_of_birth'), nil
+          assert_equal @@eleven_am.next_record(field: 'time_of_birth'), nil
         end
       end
 
       class WithIntegerField < NextRecord
         test "defaults" do
-          @six, @eight, @seven = create_three_users
-          assert_equal @six.next_record(field: 'total_friends'), @seven
-          assert_equal @seven.next_record(field: 'total_friends'), @eight
+          assert_equal @@six.next_record(field: 'total_friends'), @@seven
+          assert_equal @@seven.next_record(field: 'total_friends'), @@eight
         end
 
         test "last record returns nil" do
-          @six, @eight, @seven = create_three_users
-          assert_equal @eight.next_record(field: 'total_friends'), nil
+          assert_equal @@eight.next_record(field: 'total_friends'), nil
         end
       end
 
       class WithStringField < NextRecord
         test "defaults" do
-          @malcolm, @huey, @mlk = create_three_users
-          assert_equal @huey.next_record(field: 'name'), @malcolm
-          assert_equal @malcolm.next_record(field: 'name'), @mlk
+          assert_equal @@huey.next_record(field: 'name'), @@malcolm
+          assert_equal @@malcolm.next_record(field: 'name'), @@mlk
         end
 
         test "last record returns nil" do
-          @malcolm, @huey, @mlk = create_three_users
-          assert_equal @mlk.next_record(field: 'name'), nil
+          assert_equal @@mlk.next_record(field: 'name'), nil
         end
       end
     end
@@ -68,53 +69,45 @@ module ParamTests
     class PreviousRecord < FieldTest
       class WithDateField < PreviousRecord
         test "defaults" do
-          @feb_1965, @feb_1942, @jan_1929 = create_three_users
-          assert_equal @feb_1965.previous_record(field: 'date_of_birth'), @feb_1942
-          assert_equal @feb_1942.previous_record(field: 'date_of_birth'), @jan_1929
+          assert_equal @@feb_1965.previous_record(field: 'date_of_birth'), @@feb_1942
+          assert_equal @@feb_1942.previous_record(field: 'date_of_birth'), @@jan_1929
         end
 
-        test "last record returns nil" do
-          @feb_1965, @feb_1942, @jan_1929 = create_three_users
-          assert_equal @jan_1929.previous_record(field: 'date_of_birth'), nil
+        test "first record returns nil" do
+          assert_equal @@jan_1929.previous_record(field: 'date_of_birth'), nil
         end
       end
 
       class WithTimeField < PreviousRecord
         test "defaults" do
-          @ten_am, @nine_am, @eleven_am = create_three_users
-          assert_equal @eleven_am.previous_record(field: 'time_of_birth'), @ten_am
-          assert_equal @ten_am.previous_record(field: 'time_of_birth'), @nine_am
+          assert_equal @@eleven_am.previous_record(field: 'time_of_birth'), @@ten_am
+          assert_equal @@ten_am.previous_record(field: 'time_of_birth'), @@nine_am
         end
 
-        test "last record returns nil" do
-          @ten_am, @nine_am, @eleven_am = create_three_users
-          assert_equal @nine_am.previous_record(field: 'time_of_birth'), nil
+        test "first record returns nil" do
+          assert_equal @@nine_am.previous_record(field: 'time_of_birth'), nil
         end
       end
 
       class WithIntegerField < PreviousRecord
         test "defaults" do
-          @six, @eight, @seven = create_three_users
-          assert_equal @eight.previous_record(field: 'total_friends'), @seven
-          assert_equal @seven.previous_record(field: 'total_friends'), @six
+          assert_equal @@eight.previous_record(field: 'total_friends'), @@seven
+          assert_equal @@seven.previous_record(field: 'total_friends'), @@six
         end
 
-        test "last record returns nil" do
-          @six, @eight, @seven = create_three_users
-          assert_equal @six.previous_record(field: 'total_friends'), nil
+        test "first record returns nil" do
+          assert_equal @@six.previous_record(field: 'total_friends'), nil
         end
       end
 
       class WithStringField < PreviousRecord
         test "defaults" do
-          @malcolm, @huey, @mlk = create_three_users
-          assert_equal @mlk.previous_record(field: 'name'), @malcolm
-          assert_equal @malcolm.previous_record(field: 'name'), @huey
+          assert_equal @@mlk.previous_record(field: 'name'), @@malcolm
+          assert_equal @@malcolm.previous_record(field: 'name'), @@huey
         end
 
-        test "last record returns nil" do
-          @malcolm, @huey, @mlk = create_three_users
-          assert_equal @huey.previous_record(field: 'name'), nil
+        test "first record returns nil" do
+          assert_equal @@huey.previous_record(field: 'name'), nil
         end
       end
     end
