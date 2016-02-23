6a3862d1924d5b9304b485c207fdb4b94d57cc99
Thomas Brigham <thomas@thomasbrigham.me>
2016-02-22 22:06:23 -0800

    Refactor Nextable and Nextable::DB
    - Cleans up verbose method names (ie un-prepending db_ to all)
    - DRY up finding first/last record by ID
    - Update dummy app config to suppress deprecation warnings

--- lib/nextable/db.rb
+++ lib/nextable/db.rb
@@ -9,75 +9,60 @@ module Nextable
     ## next_record helpers
     ####
 
-    def db_next_nil
-      @scope.where("#{@field} IS NULL AND id > ?", id).order(:id).first
+    def next_nil
+      @scope.where("? IS NULL AND id > ?", @field, id).order(:id).first
     end
 
-    def db_first_non_nil
-      @scope.where("#{@field} IS NOT NULL").order(by_field).first
+    def first_non_nil
+      @scope.where("? IS NOT NULL", @field)
     end
 
-    def by_field
-      return "lower(#{@field}) asc" if field_is_string?
-      "#{@field} asc"
+    def greater_id_params
+      if field_is_string?
+        ["? = ? AND id > ?", @field.downcase, self.send(@field).downcase, id]
+      else
+        ["#{@field} = ? AND id > ?", self.send(@field), id]
+      end
     end
 
-    def db_eq_field_greater_id_params
-      return db_eq_field_next_record, db_param, id
-    end
-
-    def db_eq_field_next_record
-      return "lower(#{@field}) = ? AND id > ?" if field_is_string?
-      "#{@field} = ? AND id > ?"
-    end
-
-    def db_greater_query
-      return "lower(#{@field}) > ?" if field_is_string?
-      "#{@field} > ?"
+    def greater_field_params
+      if field_is_string?
+        ["lower(#{@field}) > ?", self.send(@field).downcase]
+      else
+        ["#{@field} > ?", self.send(@field)]
+      end
     end
 
     ####
     ## previous_record helpers
     ####
 
-    def db_prev_nil
-      @scope.where("#{@field} IS NULL AND id < ?", id).order(id: :desc).first
+    def prev_nil
+      @scope.where("? IS NULL AND id < ?", id).order(id: :desc).first
     end
 
-    def db_last_non_nil
-      @scope.where("#{@field} IS NOT NULL").order(by_field_desc).first
+    def last_non_nil
+      @scope.where("? IS NOT NULL")
     end
 
-    def by_field_desc
-      return "lower(#{@field}) desc" if field_is_string?
-      "#{@field} desc"
+    def lesser_id_params
+      if field_is_string?
+        ["lower(#{@field}) = ? AND id < ?", self.send(@field).downcase, id]
+      else
+        ["#{@field} = ? AND id < ?", self.send(@field), id]
+      end
     end
 
-    def db_eq_field_lesser_id_params
-      return db_eq_field_previous_record, db_param, id
-    end
-
-    def db_eq_field_previous_record
-      return "lower(#{@field}) = ? AND id < ?" if field_is_string?
-      "#{@field} = ? AND id < ?"
-    end
-
-    def db_lesser_query
-      return "lower(#{@field}) < ?" if field_is_string?
-      "#{@field} < ?"
+    def lesser_field_params
+      if field_is_string?
+        ["lower(#{@field}) < ?", self.send(@field).downcase]
+      else
+        ["#{@field} < ?", self.send(@field)]
+      end
     end
 
     ### Utility ###
-    def db_param
-      input = self.send(@field)
-      input.tap { |i| i.downcase! if field_is_string? }
-    end
-
     def field_is_string?
-      self.class.columns_hash[@field].type == :string
-    end
-
-    def field_is_date_or_time?
       self.class.columns_hash[@field].type == :string
     end
   end
